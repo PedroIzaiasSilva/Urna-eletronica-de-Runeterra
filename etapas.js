2675aeaf32c017e599a91766eff26fbcdec214e5
Pedro Izaias <pedroizaias.dev@gmail.com>
2022-12-13 20:13:07 -0300

    commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'COMANDANTE',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '23512',
+                nome: 'Darius',
+                partido: 'Noxus',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Comandante'}
+                ]
+            },
+            {
+                numero: '27410',
+                nome: 'Garen',
+                partido: 'Demacia',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Comandante'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'REI DE RUNETERRA',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Swain',
+                partido: 'Noxus',
+                vice: 'LeBlanc',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Rei'},
+                    {url:'99_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Jarvan IV',
+                partido: 'Demacia',
+                vice: 'Lux',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Rei'},
+                    {url:'84_2.jpg', legenda: 'Vice', small: true}
+                ]
+            },
+        ]
+    }
+];
